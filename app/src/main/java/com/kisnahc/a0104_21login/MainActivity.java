fd59cdcd014c6a53653f3a98fe5a68a7ba704c42
kisnahc <kisnahc@naver.com>
2021-01-06 00:37:21 +0900

    add bottom_nav,fragment

--- app/src/main/java/com/kisnahc/a0104_21login/MainActivity.java
+++ app/src/main/java/com/kisnahc/a0104_21login/MainActivity.java
@@ -1,14 +1,69 @@
 package com.kisnahc.a0104_21login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                BottomNavigate(item.getItemId());
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.nav_1);
+    }
+
+    private void BottomNavigate(int itemId) {
+        String tag = String.valueOf(itemId);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+
+        if (fragment == null) {
+            if (itemId == R.id.nav_1) {
+                fragment = new HomeFragment();
+
+            } else if (itemId == R.id.nav_2) {
+                fragment = new SearchFragment();
+
+            } else if (itemId == R.id.nav_3) {
+                fragment = new MapFragment();
+
+            } else {
+                fragment = new AccountFragment();
+
+            }
+            fragmentTransaction.add(R.id.content_layout, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
     }
 }
---
+++ app/src/main/java/com/kisnahc/a0104_21login/MapFragment.java
@@ -0,0 +1,24 @@
+package com.kisnahc.a0104_21login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MapFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
